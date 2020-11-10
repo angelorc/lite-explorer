af76ef234a48668946233ec999f6969e21ec3944
Angelo <angelo@rcstart.it>
2020-11-10 02:16:59 +0100

    Testing new txs table

--- pages/transactions/index.vue
+++ pages/transactions/index.vue
@@ -1,6 +1,12 @@
 <template>
   <page-template>
     <h1 class="text-h4 mb-4">Transactions</h1>
+    <app-table
+     field="transactions"
+     :items="txs.data"
+     class="mb-8"
+    >
+    </app-table>
     <transactions-data-table :transactions="txs"></transactions-data-table>
   </page-template>
 </template>
@@ -8,11 +14,13 @@
 <script>
 import PageTemplate from '@/components/PageTemplate'
 import TransactionsDataTable from '@/components/TransactionsDataTable'
+import AppTable from '@/components/app/Table'
 
 export default {
   components: {
     PageTemplate,
     TransactionsDataTable,
+    AppTable
   },
   watchQuery: ['page'],
   key: (to) => to.fullPath,
@@ -25,6 +33,6 @@ export default {
     return {
       txs,
     }
-  },
+  }
 }
 </script>
